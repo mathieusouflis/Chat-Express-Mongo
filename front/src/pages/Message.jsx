80aa0850aa8cca907d4cadb1de3c84ac0681c3d3
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-03-14 18:10:37 +0100

    :sparkles: Dockerisation

--- front/src/pages/Message.jsx
+++ front/src/pages/Message.jsx
@@ -1,31 +1,41 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import "./Message.css"
+
+const socket = io("ws://localhost:4865")
 
 const MessagePage = () => {
     const [messages, setMessages] = useState([])
-    const socket = io("ws:localhost:4865")
+    const [userNumber, setUserNumber] = useState(0)
 
     useEffect(() => {
         socket.on("message", (data) => {
             setMessages((old) => [...old, data])
         })
 
-        socket.off("message")
-    }, [socket])
+        socket.on("total_user", (data) => {
+            setUserNumber(data)
+        })
+
+        return () => {
+            socket.off("message")
+            socket.off("total_user")
+        }
+    }, [])
 
     const handleMessage = (e) => {  
         e.preventDefault()
 
-        const username = e.target[0].value.trim()
+        const name = e.target[0].value.trim()
         const message = e.target[1].value.trim()
         const now = new Date()
         
-        if(username !== "" && message !== ""){
+        if(name !== "" && message !== ""){
             const data = {
-                username,
+                name,
                 message,
-                date: now.getDate(),
-                hours: now.getTime()
+                date: `${now.getDay()}/${now.getMonth()}/${now.getFullYear()}`,
+                heure: `${now.getHours}:${now.getMinutes}:${now.getSeconds}`
             }
             
             setMessages((old) => [...old, data])
@@ -37,22 +47,27 @@ const MessagePage = () => {
     }
     
     return (
-        <>
+        <div className="container">
+        <p><strong>{userNumber}</strong> Utilisateur.s connécté.s</p>
         <form onSubmit={handleMessage}>
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" id="username" />
+            <div className="form-section">
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" id="username" className="button" />
+            </div>
             {
-                messages.map((message) => <div>
-                    <span>{message.name}</span>
+                messages.map((message, index) => <div key={index} className="message">
+                    <span><strong>{message.name}</strong></span>
                     <p>{message.message}</p>
                     <p>{message.date} {message.hours}</p>
                 </div>)
             }
-            <label htmlFor="message">Message</label>
-            <input type="text" name="message" id="message" />
-            <input type="submit" value="Send" />
+            <div className="form-section">
+                <label htmlFor="message">Message</label>
+                <input type="text" name="message" id="message" />
+                <input type="submit" value="Send" className="button" />
+            </div>
         </form>
-        </>
+        </div>
     )
 }
 
