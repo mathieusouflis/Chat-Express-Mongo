64576910475d40051eeb000dc2f4e4127220bd6f
Nabibouch <lmrb.nabil@gmail.com>
2025-03-14 13:02:47 +0100

    :white_check_mark: initialisation de socket.io + nb user connecté

---
+++ back/index.js
@@ -0,0 +1,35 @@
+//Import des modules utile
+import express from 'express';
+import { Server } from 'socket.io'; 
+import dotenv from "dotenv";
+
+
+//lancement du serveur web
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 4865;
+const server = app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+
+const io = new Server(server) ;
+let userConnected = new Set();
+
+
+//Fonction qui affiche le nombre d'utilisateur connecté
+function userEvent(socket){
+    console.log(socket.id); //Affiche le socket id dans la console
+    
+//Lorsqu'un utilisateur se connecte, son socket id est ajouté au nombre d'utilisateur connecté 
+    userConnected.add(socket.id);
+    io.emit('total_user', userConnected.size)
+
+//Lorsqu'un utilisateur se déconnecte, son socket id est supprimé du nombre d'utilisateur connecté
+    socket.on('disconnect', () =>{
+        userConnected.delete(socket.id);
+        io.emit('total_user', userConnected.size)
+
+    })
+}
+
+
+io.on('connection', userEvent);
